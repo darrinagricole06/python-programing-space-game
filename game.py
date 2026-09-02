b613692d2fc1fc30b353ebd0daa41b86942bb597
Darrin <darrinagricole06@gmail.com>
2026-09-02 18:26:01 +0400

    edit game.py

--- game.py
+++ game.py
@@ -3,6 +3,13 @@ from engine.galaxy import create_galaxy
 from engine.journey import travel
 import constants
 
+def should_stop_fn() -> bool:
+    answer = input("Do you want to continue your journey? (y/n): ")
+
+    if answer == "y":
+        return True
+    else:
+        return False
 
 def main():
     show_intro(constants.SHIP_NAME, constants.CREW_DESCRIPTION)
@@ -12,6 +19,7 @@ def main():
         constants.STARTING_OXYGEN,
         constants.STARTING_HULL,
         constants.SHIP_NAME,
+        should_stop_fn
     )
 
 
